10bb1507edc4aa9de5257e247bb92e29757d5341
teddycool21 <teddylesmana21@gmail.com>
2020-07-26 12:47:28 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() == 100) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
